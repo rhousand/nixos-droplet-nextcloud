53810d2e46478c9b9fe9cc0758e6c75ec239bead
Ryan Housand <rhousand@gmail.com>
2024-05-29 18:37:55 +0000

    Added Agenix

--- host.nix
+++ host.nix
@@ -16,7 +16,7 @@ in {
     screen
     tailscale
     tealdeer
-    #inputs.agenix.packages."${system}".default
+    inputs.agenix.packages."${system}".default
   ];
 
   age.secrets.secret1 = {
@@ -86,7 +86,7 @@ in {
     configureRedis = true;
     autoUpdateApps.enable = true;
     extraApps = {
-      inherit (config.services.nextcloud.package.packages.apps) cookbook contacts calendar tasks notes onlyoffice;
+      inherit (config.services.nextcloud.package.packages.apps) spreed cookbook contacts calendar tasks notes onlyoffice;
       ## Unsplash does not support nextcloud 28 at this time however it should be added above as extraApps when ready.
       #unsplash =  pkgs.fetchNextcloudApp rec {
       #  url = "https://github.com/nextcloud/unsplash/releases/download/v2.2.1/unsplash.tar.gz";
