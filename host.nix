7d1e082be551e843538da4abb2639a799bc284f1
Ryan Housand <rhousand@gmail.com>
2024-07-08 13:59:14 +0000

    moved cleaning from nh to nix.gc

--- host.nix
+++ host.nix
@@ -141,8 +141,8 @@ in {
 
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+    #clean.enable = true;
+    #clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/rhousand/repos/nixos-droplet-nextcloud";
   };
 
